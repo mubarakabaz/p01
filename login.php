06105323fd0c05a12f07fa2012e0cc61de20675c
mubarakabaz <abdulazizmubarak1@gmail.com>
2021-03-06 19:58:35 +0800

    kurang rapi

--- login.php
+++ login.php
@@ -5,17 +5,10 @@
     // header("refresh: 5");
     include('desain.php');
     ?>
-
-    <style>
-         body{
-             background-image: url('assets/background.jpg');
-             background-position: center;
-             z-index: 9999;
-         }
-    </style>
 </head>
 
-<body>
+<body id="login-page">
+
     <div class="container login">
     <div class="container col-sm">
         <div class="logo text-center">
@@ -38,18 +31,10 @@
             
         </form>
 
-        <?php
-        if (isset($_GET['pesan'])) {
-        if ($_GET['pesan'] == "gagal") {
-            echo "<div class='alert alert-sm alert-danger'>" . "Login gagal! username dan password salah!" . "</div>";
-        } else if ($_GET['pesan'] == "logout") {
-            echo "<div class='alert alert-primary'>" . "Anda telah berhasil logout" . "</div>";
-        } else if ($_GET['pesan'] == "belum_login") {
-            echo "<div class='alert alert-warning'>" . "Anda harus login untuk mengakses halaman admin" . "</div>";
-        }
-        }
-        ?>
+        
     </div>
+
+    
     
     <?php include('bootstrapScript.php'); ?>
     
